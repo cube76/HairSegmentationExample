9a8e5bd4db4e50d062120511aa39bb10831d5f27
tryanbot <tryan@tryanbot.com>
2022-10-18 16:13:44 +0700

    set mask

--- app/src/main/java/id/infidea/hairsegmentationexample/MainActivity.java
+++ app/src/main/java/id/infidea/hairsegmentationexample/MainActivity.java
@@ -49,6 +49,7 @@ import com.google.mediapipe.glutil.EglManager;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.List;
 
@@ -167,11 +168,26 @@ public class MainActivity extends AppCompatActivity {
               bitmap = AndroidPacketGetter.getBitmapFromRgba(packet);
               ByteArrayOutputStream stream = new ByteArrayOutputStream();
 //              bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-              Bitmap newBitmap = Bitmap.createScaledBitmap(bitmap, 1, 1, true);
-              final int color = newBitmap.getPixel(0, 0);
+//              Bitmap newBitmap = Bitmap.createScaledBitmap(bitmap, 1, 1, true);
+//              final int color = newBitmap.getPixel(0, 0);
 //              byte[] byteArray = stream.toByteArray();
 //              bitmap.recycle();
-              Log.v(TAG, "Received bitmap: " + color);
+              int width = bitmap.getWidth();
+              int height = bitmap.getHeight();
+
+              int size = bitmap.getRowBytes() * bitmap.getHeight();
+              ByteBuffer byteBuffer = ByteBuffer.allocate(size);
+              bitmap.copyPixelsToBuffer(byteBuffer);
+              byte[] byteArray = byteBuffer.array();
+              Log.v(TAG, "Received bitmap1: " + Arrays.toString(byteArray));
+//
+//              Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+//                @RequiresApi(api = Build.VERSION_CODES.O)
+//                public void onGenerated(Palette p) {
+//                  // Use generated instance
+//                  Log.v(TAG, "Received bitmap: " + p.getSwatches());
+//                }
+//              });
             });
       processor.addPacketCallback(
               "output_size",
